fdebfb9a05979813471f27328d2501838b78fc2d
yashita <yashitakhandelwal12@gmail.com>
2024-02-11 16:07:25 +0530

    update app,js

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //rendering frontend
-// app.use(express.static(path.join(__dirname)));
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
+app.use(express.static(path.join(__dirname,'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'build', 'index.html'));
+});
 
 
 const PORT = 5000;
